68a8f71f04d42db45590546c220ce82448731297
Vanessa Martinez <vmartinez.contact@gmail.com>
2017-07-26 16:23:27 -0700

    address bar works

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 require("./lib/social");
-require("./lib/geolocation");
 require("./lib/ads");
+var geolocation = require("./lib/geolocation");
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
@@ -12,6 +12,10 @@ var regionFilter = $.one("select.regionsList");
 var trailRegion = $("[region]");
 var milesFilter = $.one("select.milesFilter");
 var trailMiles = $("[miles]");
+
+var gpsLocator = $.one("img.gpsIcon");
+var locationInput = $.one("input.address");
+var addressSearch = $.one("button.addressGo");
 
 var mapElement = $.one("leaflet-map");
 var map = mapElement.map;
@@ -36,6 +40,61 @@ var size = 16;
 // })
 
 // filter by region
+
+var LAT_DEGREE = 69;
+var LONG_DEGREE = Math.cos(47 / 360 * Math.PI * 2) * 69;
+
+var sortByDistance = function([lat, lng]) {
+  var trails = $(".trail").map(function(element) {
+    var map = $.one("leaflet-map", element);
+    return {
+      element,
+      lat: map.getAttribute("lat") * 1,
+      lng: map.getAttribute("lng") * 1,
+      distance: 0
+    }
+  });
+
+  trails.forEach(function(t) {
+    var xDistance = Math.abs(lng - t.lng) * LONG_DEGREE;
+    var yDistance = Math.abs(lat - t.lat) * LAT_DEGREE;
+
+    t.distance = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  });
+
+  // trails.forEach(t => t.distance = (Math.random() * 100) | 0);
+  trails.sort((a, b) => a.distance - b.distance);
+  var container = $.one(".trail-info");
+  // console.table(trails);
+  trails.forEach(t => container.appendChild(t.element));
+};
+
+var getLocation = function(e) {
+  e.preventDefault();
+  locationInput.value = "Determining your location...";
+
+  geolocation.gps(function(err, coords) {
+    if (err) return console.log(err);
+    // console.log(coords);
+    locationInput.value = coords.map(n => n.toFixed(3)).join(", ");
+    sortByDistance(coords);
+  });
+};
+
+var getAddress = function(e) {
+  geolocation.address(e.value, function(err, coords) {
+    if (err) return console.log(err);
+    sortByDistance(coords);
+  })
+};
+
+var returnKey = function(r) {
+  if (r.keyCode == 13) {
+    var address = this.value;
+    getAddress(this);
+  };
+};
+
 var filterByRegion = function(list) {
   var value = regionFilter.value;
   if (!value) return;
@@ -78,6 +137,12 @@ var applyFilters = function() {
   [filterByMiles, filterByRegion, showHide].forEach(f => f(filtered));
 };
 
+
+
+addressSearch.addEventListener("click", getAddress);
+locationInput.addEventListener("keydown", returnKey)
+gpsLocator.addEventListener("click", getLocation);
 regionFilter.addEventListener("change", applyFilters);
 milesFilter.addEventListener("change", applyFilters);
+
 applyFilters();
